0bbdcf5f7ff3679ab1c60541eb497f54ce6b90cd
yq <yan_qiang_1@163.com>
2019-07-31 22:43:23 +0800

    first commit

---
+++ src/main/java/com/yq/wx/WxSdkToolApplication.java
@@ -0,0 +1,13 @@
+package com.yq.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSdkToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSdkToolApplication.class, args);
+    }
+
+}
